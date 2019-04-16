56e92254bc7e24570edcc1e91c329ab5052ace50
Hercules Santos <hercules.eduardo@gmail.com>
2019-04-15 23:10:26 -0300

    formatting

--- Todo.js
+++ Todo.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const mongoose_delete = require('mongoose-delete')
+const mongooseDelete = require('mongoose-delete')
 
 const TodoSchema = new Schema({
-    task: String
+  task: String
 })
 
-//TodoSchema.plugin(mongoose_delete)
+// TodoSchema.plugin(mongooseDelete)
 
 // Override all methods
-TodoSchema.plugin(mongoose_delete, { overrideMethods: 'all' });
-// or 
-// TodoSchema.plugin(mongoose_delete, { overrideMethods: true });
+TodoSchema.plugin(mongooseDelete, { overrideMethods: 'all' })
+// or
+// TodoSchema.plugin(mongooseDelete, { overrideMethods: true })
 
 // Overide only specific methods
-// TodoSchema.plugin(mongoose_delete, { overrideMethods: ['count', 'find', 'findOne', 'findOneAndUpdate', 'update'] });
+// TodoSchema.plugin(mongooseDelete, { overrideMethods: ['count', 'find', 'findOne', 'findOneAndUpdate', 'update'] })
 // or
-// TodoSchema.plugin(mongoose_delete, { overrideMethods: ['count', 'countDocuments', 'find'] });
+// TodoSchema.plugin(mongooseDelete, { overrideMethods: ['count', 'countDocuments', 'find'] })
 // or (unrecognized method names will be ignored)
-// TodoSchema.plugin(mongoose_delete, { overrideMethods: ['count', 'find', 'errorXyz'] });
+// TodoSchema.plugin(mongooseDelete, { overrideMethods: ['count', 'find', 'errorXyz'] })
 
 const Todo = mongoose.model('Todo', TodoSchema)
 
